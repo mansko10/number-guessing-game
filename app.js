b0166e3b27525b4b8e02c2fa7724be51666073b3
mansko10 <amannan2132@gmail.com>
2023-09-27 19:18:17 +0600

    Provide feedback to user depending on if guess is right or wrong

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let hint = document.querySelector('.hint');
 
 let randomNumber = Math.ceil(Math.random() * 100);
 
+console.log(randomNumber);
+
 form.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -14,6 +16,18 @@ form.addEventListener('submit', e => {
     let userGuess = parseInt(e.target.userGuess.value);
 
     previousGuesses.innerHTML += `<span> ${userGuess} </span>`;
+
+    if (userGuess !== randomNumber) {
+        feedback.textContent = 'WRONG!';
+
+        feedback.style.backgroundColor = 'red';
+        feedback.style.color = 'white';
+    } else {
+        feedback.textContent = 'Congratulations! You got it right!';
+
+        feedback.style.backgroundColor = 'green';
+        feedback.style.color = 'white';
+    }
 
     form.reset();
 })
