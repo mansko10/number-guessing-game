f76393341e27f82f7366dda3eb0a7e23e70ead08
mansko10 <amannan2132@gmail.com>
2023-09-27 19:09:52 +0600

    Ensure that user input is stored as a number

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', e => {
 
     guidance.style.display = 'block';
 
-    let userGuess = e.target.userGuess.value;
+    let userGuess = parseInt(e.target.userGuess.value);
 
     previousGuesses.innerHTML += `<span> ${userGuess} </span>`;
 
