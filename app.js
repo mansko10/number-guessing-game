f35c42383911d9e99616ac7e6d80e64628a508e8
mansko10 <amannan2132@gmail.com>
2023-09-27 18:30:35 +0600

    Use query selectors in app.js
    Use query selectors in app.js to select relevant html elements from index.html

---
+++ app.js
@@ -0,0 +1,5 @@
+let form = document.querySelector('form');
+let guidance = document.querySelector('.guidance');
+let previousGuesses = document.querySelector('.previousGuess');
+let feedback = document.querySelector('.feedback');
+let hint = document.querySelector('.hint');
